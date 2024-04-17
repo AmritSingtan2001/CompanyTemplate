068033e9ff632b6519b695aab53db166087d393a
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-17 10:39:11 +0545

    feat(Slider):"Banner added "

---
+++ company/Company/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+const Loader = () => {
+  return <Spinner animation="grow" />;
+}
+
+export default Loader;
--- company/Company/src/components/Slider.jsx
+++ company/Company/src/components/Slider.jsx
@@ -1,34 +1,56 @@
 import Carousel from "react-bootstrap/Carousel";
 import Images from "../assets/images/bannar1.jpg";
 import Image1 from "../assets/images/bannar2.jpg";
+import Loader from "../components/Loader";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 
 const Slider =() =>{
+  const[banner, setBanner] = useState([]);
+  const[loading, setLoading]= useState(false);
+
+  useEffect(()=>{
+    FetchBanner()
+  },[])
+
+
+  const FetchBanner = async() =>{
+    try {
+      setLoading(true);
+      const response = await axios.get("http://192.168.18.187:8000/api/v1/banner/");
+      setBanner(response.data)
+      // console.log("hello i am down")
+      setLoading(false);
+      
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  if(loading){
+    return <Loader/>
+  }
+
   return (
     <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-        <img fluid 
-          className="d-block w-100 object-fit-contain"
-          src={Images}
-          style={{ maxHeight:'450px' }} 
-          alt="First slide"
-        />
-        <Carousel.Caption className="bg-dark text-white">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img fluid 
-          className="d-block w-100 object-fit-contain"
-          src={Image1}
-          style={{ maxHeight:'450px' }}
-          alt="Second slide"
-        />
-        <Carousel.Caption className="bg-dark text-white">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {
+        banner.map((item)=> ( //for i in range(4)
+          <Carousel.Item>
+            <img fluid 
+              className="d-block w-100 object-fit-cover"
+              src={item.image}
+              style={{ maxHeight:'450px' }} 
+              alt="First slide"
+            />
+            <Carousel.Caption className="bg-dark text-white">
+              <h5>{item.title}</h5>
+              <p>{item.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))
+      }
       
     </Carousel>
   );
